00b01f430dc63fc668ed1d1b711566c160651889
Starzu <m.starzec@avsystem.com>
2016-06-13 11:04:07 +0200

    I18n demos - selenium tests

--- guide/src/main/scala/io/udash/web/guide/views/ext/demo/DynamicRemoteTranslationsDemo.scala
+++ guide/src/main/scala/io/udash/web/guide/views/ext/demo/DynamicRemoteTranslationsDemo.scala
@@ -20,8 +20,8 @@ object DynamicRemoteTranslationsDemo {
     implicit val translationProvider = new RemoteTranslationProvider(serverRpc.demos().translations(), Some(LocalStorage), 6 hours)
     implicit val lang = LangProperty(Lang("en"))
     div(id := "dynamic-rpc-translations-demo", GuideStyles.frame)(
-      button(BootstrapStyles.btn, BootstrapStyles.btnPrimary)(onclick := ((_: Event) => lang.set(Lang("en"))))("EN"), " ",
-      button(BootstrapStyles.btn, BootstrapStyles.btnPrimary)(onclick := ((_: Event) => lang.set(Lang("pl"))))("PL"),
+      button(BootstrapStyles.btn, BootstrapStyles.btnPrimary)(id := "enButton", onclick := ((_: Event) => lang.set(Lang("en"))))("EN"), " ",
+      button(BootstrapStyles.btn, BootstrapStyles.btnPrimary)(id := "plButton", onclick := ((_: Event) => lang.set(Lang("pl"))))("PL"),
       ul(
         li("auth.loginLabel: ", translatedDynamic(Translations.auth.loginLabel)(_.apply())),
         li("auth.passwordLabel: ", translatedDynamic(Translations.auth.passwordLabel)(_.apply())),
