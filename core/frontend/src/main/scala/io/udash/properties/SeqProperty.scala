6b36dbfb38f65ac270e3a31f34f11d3b39be1185
Starzu <m.starzec@avsystem.com>
2016-07-20 14:51:52 +0200

    SeqProperty - reversed()

--- core/frontend/src/main/scala/io/udash/properties/SeqProperty.scala
+++ core/frontend/src/main/scala/io/udash/properties/SeqProperty.scala
@@ -53,6 +53,10 @@ trait ReadableSeqProperty[A, +ElemType <: ReadableProperty[A]] extends ReadableP
   def transform[B](transformer: A => B): ReadableSeqProperty[B, ReadableProperty[B]] =
     new TransformedReadableSeqProperty[A, B, ReadableProperty[B], ReadableProperty[A]](this, transformer, PropertyCreator.newID())
 
+  /** Creates `ReadableSeqProperty[A]` providing reversed order of elements from `this`. */
+  def reversed(): ReadableSeqProperty[A, ReadableProperty[A]] =
+    new ReversedReadableSeqProperty[A](this)
+
   /** Filters ReadableSeqProperty[A].
     *
     * @return New ReadableSeqProperty[A] with matched elements, which will be synchronised with original ReadableSeqProperty[A]. */
@@ -65,7 +69,7 @@ trait ReadableSeqProperty[A, +ElemType <: ReadableProperty[A]] extends ReadableP
                                       p: ReadableProperty[B], override val executionContext: ExecutionContext)
       extends ReadableSeqProperty[O, ReadableProperty[O]] {
 
-      override val id: UUID = UUID.randomUUID()
+      override val id: UUID = PropertyCreator.newID()
       override protected[properties] val parent: ReadableProperty[_] = null
 
       private val children = mutable.ListBuffer.empty[ReadableProperty[O]]
@@ -160,6 +164,54 @@ class TransformedReadableSeqProperty[A, B, +ElemType <: ReadableProperty[B], Ori
     origin.executionContext
 }
 
+abstract class BaseReversedSeqProperty[A, +ElemType <: ReadableProperty[A], OriginType <: ReadableSeqProperty[A, ElemType]](origin: OriginType)
+  extends ReadableSeqProperty[A, ReadableProperty[A]] {
+
+  override val id: UUID = PropertyCreator.newID()
+  override protected[properties] val parent: ReadableProperty[_] = null
+  override implicit protected[properties] def executionContext: ExecutionContext = origin.executionContext
+
+  override def get: Seq[A] =
+    origin.get.reverse
+
+  override def elemProperties: Seq[ElemType] =
+    origin.elemProperties.reverse
+
+  override def listen(l: (Seq[A]) => Any): Registration =
+    origin.listen(s => l(s.reverse))
+}
+
+class ReversedReadableSeqProperty[A](origin: ReadableSeqProperty[A, ReadableProperty[A]])
+  extends BaseReversedSeqProperty[A, ReadableProperty[A], ReadableSeqProperty[A, ReadableProperty[A]]](origin) {
+
+  override def listenStructure(l: (Patch[ReadableProperty[A]]) => Any): Registration =
+    origin.listenStructure((patch) => l(patch.copy(
+      idx = origin.size - patch.idx - patch.added.size,
+      removed = patch.removed.reverse,
+      added = patch.added.reverse
+    )))
+}
+
+class ReversedSeqProperty[A](origin: SeqProperty[A, Property[A]])
+  extends BaseReversedSeqProperty[A, Property[A], SeqProperty[A, Property[A]]](origin) with SeqProperty[A, Property[A]] {
+
+  override def setInitValue(t: Seq[A]): Unit =
+    origin.setInitValue(t.reverse)
+
+  override def set(t: Seq[A]): Unit =
+    origin.set(t.reverse)
+
+  override def replace(idx: Int, amount: Int, values: A*): Unit =
+    origin.replace(origin.size - idx, amount, values.reverse:_*)
+
+  override def listenStructure(l: (Patch[Property[A]]) => Any): Registration =
+    origin.listenStructure((patch) => l(patch.copy(
+      idx = origin.size - patch.idx - patch.added.size,
+      removed = patch.removed.reverse,
+      added = patch.added.reverse
+    )))
+}
+
 class FilteredSeqProperty[A, ElemType <: ReadableProperty[A]]
   (origin: ReadableSeqProperty[A, _ <: ElemType],
    matcher: A => Boolean,
@@ -236,9 +288,7 @@ class FilteredSeqProperty[A, ElemType <: ReadableProperty[A]]
 
   override def listenStructure(l: (Patch[ElemType]) => Any): Registration = {
     structureListeners.add(l)
-    new Registration {
-      override def cancel(): Unit = structureListeners.remove(l)
-    }
+    new PropertyRegistration(structureListeners, l)
   }
 
   override def elemProperties: Seq[ElemType] =
@@ -293,6 +343,10 @@ trait SeqProperty[A, +ElemType <: Property[A]] extends ReadableSeqProperty[A, El
     * @return New SeqProperty[B], which will be synchronised with original SeqProperty[A]. */
   def transform[B](transformer: A => B, revert: B => A): SeqProperty[B, Property[B]] =
     new TransformedSeqProperty[A, B](this, transformer, revert, PropertyCreator.newID())
+
+  /** Creates `SeqProperty[A]` providing reversed order of elements from `this`. */
+  override def reversed(): SeqProperty[A, Property[A]] =
+    new ReversedSeqProperty[A](this)
 }
 
 class TransformedSeqProperty[A, B](origin: SeqProperty[A, Property[A]], transformer: A => B, revert: B => A, override val id: UUID)
