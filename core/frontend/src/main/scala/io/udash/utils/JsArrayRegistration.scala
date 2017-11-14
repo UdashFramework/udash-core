93a64f5685ac3307580cdf633f484ffb2e740861
Starzu <m.starzec@avsystem.com>
2017-11-14 09:25:02 +0100

    JsArrayRegistration - removed synchronization

--- core/frontend/src/main/scala/io/udash/utils/JsArrayRegistration.scala
+++ core/frontend/src/main/scala/io/udash/utils/JsArrayRegistration.scala
@@ -3,7 +3,7 @@ package io.udash.utils
 import scala.scalajs.js
 
 private[udash] class JsArrayRegistration[ElementType](s: js.Array[ElementType], el: ElementType) extends Registration {
-  override def cancel(): Unit = s.synchronized { s -= el }
-  override def restart(): Unit = s.synchronized { s += el }
-  override def isActive: Boolean = s.synchronized { s.contains(el) }
+  override def cancel(): Unit = s -= el
+  override def restart(): Unit = s += el
+  override def isActive: Boolean = s.contains(el)
 }
