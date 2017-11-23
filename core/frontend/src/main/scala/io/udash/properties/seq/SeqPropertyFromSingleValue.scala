936091d97d04370414363f22a83ae5aa21799da8
ddworak <dawid.dworak@gmail.com>
2017-11-23 17:07:35 +0100

    Use js.Array in SeqPropertyFromSingleValue.

--- core/frontend/src/main/scala/io/udash/properties/seq/SeqPropertyFromSingleValue.scala
+++ core/frontend/src/main/scala/io/udash/properties/seq/SeqPropertyFromSingleValue.scala
@@ -7,6 +7,7 @@ import io.udash.properties.single._
 import io.udash.utils.{Registration, SetRegistration}
 
 import scala.collection.mutable
+import scala.scalajs.js
 
 private[properties]
 abstract class BaseReadableSeqPropertyFromSingleValue[A, B: ModelValue]
@@ -17,7 +18,7 @@ abstract class BaseReadableSeqPropertyFromSingleValue[A, B: ModelValue]
   override protected[properties] def parent: ReadableProperty[_] = null
 
   protected val structureListeners: mutable.Set[Patch[Property[B]] => Any] = mutable.Set()
-  protected val children = mutable.ListBuffer.empty[Property[B]]
+  protected val children: js.Array[Property[B]] = js.Array[Property[B]]()
 
   update(origin.get)
   origin.listen(update)
@@ -42,11 +43,11 @@ abstract class BaseReadableSeqPropertyFromSingleValue[A, B: ModelValue]
 
     val patch = if (transformed.size > current.size) {
       val added: Seq[CastableProperty[B]] = Seq.fill(transformed.size - current.size)(implicitly[PropertyCreator[B]].newProperty(this))
-      children.insertAll(commonBegin, added)
-      Some(Patch(commonBegin, Seq(), added, false))
+      children.splice(commonBegin, 0, added: _*)
+      Some(Patch(commonBegin, Seq(), added, clearsProperty = false))
     } else if (transformed.size < current.size) {
-      val removed = children.slice(commonBegin, commonBegin + current.size - transformed.size)
-      children.remove(commonBegin, current.size - transformed.size)
+      val removed = children.jsSlice(commonBegin, commonBegin + current.size - transformed.size)
+      children.splice(commonBegin, current.size - transformed.size)
       Some(Patch(commonBegin, removed, Seq(), transformed.isEmpty))
     } else None
 
