c20e13fa434f56974d971f4af3a7477ab9aed95e
Starzu <m.starzec@avsystem.com>
2018-03-21 07:26:36 +0100

    Jawn 0.11.1

--- rpc/shared/.jvm/src/main/scala/io/udash/rpc/DefaultUdashSerialization.scala
+++ rpc/shared/.jvm/src/main/scala/io/udash/rpc/DefaultUdashSerialization.scala
@@ -5,11 +5,13 @@ import io.udash.rpc.serialization.{JsonInput, JsonOutput}
 import io.udash.rpc.serialization.jawn.JawnFacade
 import jawn.Parser
 
+import scala.util.control.NonFatal
+
 /** Provides JAWN based serialization to JSON `String`. */
 trait DefaultUdashSerialization {
   def inputSerialization(value: String): Input =
     try { new JsonInput(Parser.parseFromString(value)(JawnFacade).get) }
-    catch { case ex: jawn.ParseException => throw new GenCodec.ReadFailure("JAWN parse error!", ex) }
+    catch { case NonFatal(ex) => throw new GenCodec.ReadFailure("JAWN parse error!", ex) }
 
   def outputSerialization(valueConsumer: String => Unit): Output =
     new JsonOutput(valueConsumer)
--- rpc/shared/.jvm/src/main/scala/io/udash/rpc/serialization/jawn/JawnFacade.scala
+++ rpc/shared/.jvm/src/main/scala/io/udash/rpc/serialization/jawn/JawnFacade.scala
@@ -2,6 +2,8 @@ package io.udash.rpc.serialization.jawn
 
 import jawn.SimpleFacade
 
+import scala.util.Try
+
 object JawnFacade extends SimpleFacade[JValue] {
   override def jnull() = JNull
 
@@ -9,16 +11,14 @@ object JawnFacade extends SimpleFacade[JValue] {
 
   override def jfalse() = JBoolean(false)
 
-  override def jnum(s: String): JNum =
-    try { JInt(s.toInt) }
-    catch {
-      case _: NumberFormatException =>
-        JDouble(s.toDouble)
-    }
+  override def jnum(s: CharSequence, decIndex: Int, expIndex: Int): JValue = {
+    val str = s.toString
+    if (decIndex == -1 && expIndex == -1) {
+      Try(JInt(str.toInt)).getOrElse(JDouble(str.toDouble))
+    } else JDouble(str.toDouble)
+  }
 
-  override def jint(s: String): JNum = jnum(s)
-
-  override def jstring(s: String) = JString(s)
+  override def jstring(s: CharSequence): JValue = JString(s.toString)
 
   override def jarray(vs: List[JValue]) = JList(vs)
 
