ce815ead189b5ab8d45536590476498c5196bb8f
ghik <romeqjanoosh@gmail.com>
2018-05-07 11:47:00 +0200

    build fixes, cosmetic changes

--- rpc/shared/src/main/scala/io/udash/rpc/serialization/ExceptionCodecRegistry.scala
+++ rpc/shared/src/main/scala/io/udash/rpc/serialization/ExceptionCodecRegistry.scala
@@ -24,19 +24,19 @@ abstract class ClassNameBasedECR extends ExceptionCodecRegistry {
   protected final val exceptionWriter: (Output, Throwable) => Unit =
     (output: Output, ex: Throwable) => if (ex.getMessage == null) output.writeNull() else output.writeString(ex.getMessage)
 
-  protected final val exceptionReader: (Input) => String =
+  protected final val exceptionReader: Input => String =
     (input: Input) => if (input.inputType == InputType.Null) input.readNull() else input.readString()
 
-  register(GenCodec.create((input) => new NullPointerException(exceptionReader(input)), exceptionWriter))
-  register(GenCodec.create((input) => new ClassCastException(exceptionReader(input)), exceptionWriter))
-  register(GenCodec.create((input) => new IndexOutOfBoundsException(exceptionReader(input)), exceptionWriter))
-  register(GenCodec.create((input) => new ArrayIndexOutOfBoundsException(exceptionReader(input)), exceptionWriter))
-  register(GenCodec.create((input) => new StringIndexOutOfBoundsException(exceptionReader(input)), exceptionWriter))
-  register(GenCodec.create((input) => new UnsupportedOperationException(exceptionReader(input)), exceptionWriter))
-  register(GenCodec.create((input) => new IllegalArgumentException(exceptionReader(input)), exceptionWriter))
-  register(GenCodec.create((input) => new IllegalStateException(exceptionReader(input)), exceptionWriter))
-  register(GenCodec.create((input) => new NoSuchElementException(exceptionReader(input)), exceptionWriter))
-  register(GenCodec.create((input) => new NumberFormatException(exceptionReader(input)), exceptionWriter))
+  register(GenCodec.create(input => new NullPointerException(exceptionReader(input)), exceptionWriter))
+  register(GenCodec.create(input => new ClassCastException(exceptionReader(input)), exceptionWriter))
+  register(GenCodec.create(input => new IndexOutOfBoundsException(exceptionReader(input)), exceptionWriter))
+  register(GenCodec.create(input => new ArrayIndexOutOfBoundsException(exceptionReader(input)), exceptionWriter))
+  register(GenCodec.create(input => new StringIndexOutOfBoundsException(exceptionReader(input)), exceptionWriter))
+  register(GenCodec.create(input => new UnsupportedOperationException(exceptionReader(input)), exceptionWriter))
+  register(GenCodec.create(input => new IllegalArgumentException(exceptionReader(input)), exceptionWriter))
+  register(GenCodec.create(input => new IllegalStateException(exceptionReader(input)), exceptionWriter))
+  register(GenCodec.create(input => new NoSuchElementException(exceptionReader(input)), exceptionWriter))
+  register(GenCodec.create(input => new NumberFormatException(exceptionReader(input)), exceptionWriter))
 
   override def register[T <: Throwable : ClassTag](codec: GenCodec[T]): Unit =
     codecs(classTag[T].runtimeClass.getName) = codec
