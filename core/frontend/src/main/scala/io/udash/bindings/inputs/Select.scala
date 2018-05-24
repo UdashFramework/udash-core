d142689b4e2bf042f333bfc0dd9135d7c14676b2
Starzu <m.starzec@avsystem.com>
2018-05-24 11:15:53 +0200

    Select - API refactoring

--- core/frontend/src/main/scala/io/udash/bindings/inputs/Select.scala
+++ core/frontend/src/main/scala/io/udash/bindings/inputs/Select.scala
@@ -1,8 +1,11 @@
 package io.udash.bindings.inputs
 
+import io.udash._
+import io.udash.properties.PropertyCreator
 import io.udash.properties.seq.SeqProperty
-import io.udash.properties.single.{Property, ReadableProperty}
-import org.scalajs.dom.{html, _}
+import org.scalajs.dom.html.{Option, Select}
+import org.scalajs.dom.raw.HTMLOptionElement
+import org.scalajs.dom.{Element, Event}
 import scalatags.JsDom
 import scalatags.JsDom.all._
 
@@ -15,15 +18,93 @@ object Select {
   /**
     * Single select for ValueProperty.
     *
+    * @param selectedItems Property to bind.
+    * @param options SeqProperty of available options.
+    * @param label Provides element's label.
+    * @param xs Additional Modifiers for the select tag, don't use modifiers on value, onchange and selected attributes.
+    * @return Binding with `select` element, which can be used as Scalatags modifier.
+    */
+  def apply[T : PropertyCreator](
+    selectedItems: Property[T], options: ReadableProperty[Seq[T]]
+  )(label: T => Modifier, xs: Modifier*): InputBinding[Select] = {
+    new InputBinding[Select] {
+      private val selector = select(xs)(
+        nestedInterceptor(
+          produceWithNested(options) { case (opts, nested) =>
+            if (opts.nonEmpty && !opts.contains(selectedItems.get)) {
+              selectedItems.set(opts.head)
+            }
+
+            opts.zipWithIndex.map { case (opt, idx) =>
+              option(
+                value := idx.toString,
+                nested((selected := "selected").attrIf(selectedItems.transform(_ == opt)))
+              )(label(opt)).render
+            }
+          }
+        )
+      ).render
+
+      selector.onchange = (_: Event) => selectedItems.set(options.get.apply(selector.value.toInt))
+
+      override def render: Select = selector
+    }
+  }
+
+  /**
+    * Multi selection for SeqProperty. Bound SeqProperty will contain selected options.
+    *
+    * @param selectedItems Property to bind.
+    * @param options SeqProperty of available options.
+    * @param label Provides element label.
+    * @param xs Additional Modifiers, don't use modifiers on value, onchange and selected attributes.
+    * @return Binding with `select` element, which can be used as Scalatags modifier.
+    */
+  def apply[T : PropertyCreator, ElemType <: Property[T]](
+    selectedItems: SeqProperty[T, ElemType], options: ReadableProperty[Seq[T]]
+  )(label: T => Modifier, xs: Modifier*): InputBinding[Select] = {
+    new InputBinding[Select] {
+      private val selector = select(multiple := true, xs)(
+        nestedInterceptor(
+          produceWithNested(options) { case (opts, nested) =>
+            selectedItems.set(selectedItems.get.filter(opts.contains))
+
+            opts.zipWithIndex.map { case (opt, idx) =>
+              option(
+                value := idx.toString,
+                nested((selected := "selected").attrIf(selectedItems.transform(_.contains(opt))))
+              )(label(opt)).render
+            }
+          }
+        )
+      ).render
+
+      selector.onchange = (_: Event) => {
+        val opts = options.get
+        val selectedNodes = selector.querySelectorAll("option:checked")
+        val selection = (0 until selectedNodes.length).map { idx =>
+          opts(selectedNodes(idx).asInstanceOf[HTMLOptionElement].value.toInt)
+        }
+        selectedItems.set(selection)
+      }
+
+      override def render: Select = selector
+    }
+  }
+
+  /**
+    * Single select for ValueProperty.
+    *
     * @param property Property to bind.
     * @param options Seq of available options.
     * @param label Provides element label.
     * @param xs Additional Modifiers, don't use modifiers on value, onchange and selected attributes.
     * @return HTML select tag with bound ValueProperty, applied modifiers and nested options.
     */
+  @deprecated("Use the constructor with dynamic options set and generic element type.", "0.7.0")
   def apply(
     property: Property[String], options: Seq[String], label: String => Modifier
-  )(xs: Modifier*): JsDom.TypedTag[html.Select] = {
+  )(xs: Modifier*): JsDom.TypedTag[Select] = {
     val htmlOptions = prepareHtmlOptions(options, label)
 
     def refreshSelectedItems(): Unit = {
@@ -34,7 +115,7 @@ object Select {
 
     val bind = new JsDom.Modifier {
       override def applyTo(t: Element): Unit = {
-        val element = t.asInstanceOf[html.Select]
+        val element = t.asInstanceOf[Select]
 
         refreshSelectedItems()
         property.listen(_ => refreshSelectedItems())
@@ -56,9 +137,10 @@ object Select {
     * @param xs Additional Modifiers, don't use modifiers on value, onchange and selected attributes.
     * @return HTML select tag with bound SeqProperty, applied modifiers and nested options.
     */
+  @deprecated("Use the constructor with dynamic options set and generic element type.", "0.7.0")
   def apply(
     property: SeqProperty[String, _ <: ReadableProperty[String]], options: Seq[String], label: String => Modifier
-  )(xs: Modifier*): JsDom.TypedTag[html.Select] = {
+  )(xs: Modifier*): JsDom.TypedTag[Select] = {
     val htmlOptions = prepareHtmlOptions(options, label)
 
     def refreshSelectedItems(): Unit = {
@@ -74,7 +156,7 @@ object Select {
 
     val bind = new JsDom.Modifier {
       override def applyTo(t: Element): Unit = {
-        val element = t.asInstanceOf[html.Select]
+        val element = t.asInstanceOf[Select]
 
         refreshSelectedItems()
         property.listen(_ => refreshSelectedItems())
@@ -85,7 +167,7 @@ object Select {
     select(multiple := true, bind, xs)(htmlOptions)
   }
 
-  private def prepareHtmlOptions(options: Seq[String], label: String => Modifier): Seq[html.Option] =
+  private def prepareHtmlOptions(options: Seq[String], label: String => Modifier): Seq[Option] =
     options.map { opt =>
       option(value := opt)(label(opt)).render
     }
--- core/frontend/src/test/scala/io/udash/bindings/inputs/SelectTest.scala
+++ core/frontend/src/test/scala/io/udash/bindings/inputs/SelectTest.scala
@@ -1,17 +1,18 @@
 package io.udash.bindings.inputs
 
+import com.github.ghik.silencer.silent
 import io.udash._
-import io.udash.bindings.inputs
+import io.udash.wrappers.jquery._
 import io.udash.properties.seq.SeqProperty
 import io.udash.testing.UdashFrontendTest
 
 class SelectTest extends UdashFrontendTest {
   "Select" should {
-    "synchronise state with property changes" in {
+    "synchronise state with property changes (deprecated)" in {
       val options = Seq("A", "B", "C", "D", "E")
       val p = Property[String]("B")
 
-      val select = inputs.Select(p, options, inputs.Select.defaultLabel)().render
+      val select = (Select(p, options, Select.defaultLabel)(): @silent).render
 
       select.childElementCount should be(5)
       select.value should be("B")
@@ -22,11 +23,11 @@ class SelectTest extends UdashFrontendTest {
       }
     }
 
-    "synchronise property with state changes" in {
+    "synchronise property with state changes (deprecated)" in {
       val options = Seq("A", "B", "C", "D", "E")
       val p = Property[String]("B")
 
-      val select = inputs.Select(p, options, inputs.Select.defaultLabel)().render
+      val select = (Select(p, options, Select.defaultLabel)(): @silent).render
 
       select.childElementCount should be(5)
 
@@ -37,13 +38,87 @@ class SelectTest extends UdashFrontendTest {
         p.get should be(o)
       }
     }
-  }
-  "Select with multiple on" should {
+
     "synchronise state with property changes" in {
+      val options = Seq("A", "B", "C", "D", "E")
+      val p = Property[String]("B")
+
+      val select = Select(p, options.toProperty)(Select.defaultLabel).render
+
+      select.childElementCount should be(5)
+      select.value should be("1")
+
+      for ((o, idx) <- options.zipWithIndex) {
+        p.set(o)
+        select.value should be(idx.toString)
+      }
+    }
+
+    "synchronise property with state changes" in {
+      val options = Seq("A", "B", "C", "D", "E")
+      val p = Property[String]("B")
+
+      val select = Select(p, options.toProperty)(Select.defaultLabel).render
+
+      select.childElementCount should be(5)
+
+      for ((o, idx) <- options.zipWithIndex) {
+        select.value = idx.toString
+        jQ(select).change()
+        p.get should be(o)
+      }
+    }
+
+    "synchronise visible options" in {
+      val options = SeqProperty("A", "B", "C", "D", "E")
+      val p = Property[String]("B")
+
+      val select = Select(p, options)(Select.defaultLabel)
+      val selectElement = select.render
+
+      selectElement.childElementCount should be(5)
+      p.listenersCount() should be(5)
+
+      for ((o, idx) <- options.get.zipWithIndex) {
+        p.set(o)
+        selectElement.value should be(idx.toString)
+      }
+
+      options.remove(2, 3)
+      selectElement.childElementCount should be(2)
+      p.listenersCount() should be(2)
+      p.get should be("A")
+      selectElement.value should be("0")
+
+      options.prepend("1", "2")
+      selectElement.childElementCount should be(4)
+      p.listenersCount() should be(4)
+      p.get should be("A")
+      selectElement.value should be("2")
+
+      options.clear()
+      selectElement.childElementCount should be(0)
+      p.listenersCount() should be(0)
+      p.get should be("A")
+      selectElement.value should be("")
+
+      options.append("x", "y", "z")
+      selectElement.childElementCount should be(3)
+      p.listenersCount() should be(3)
+      p.get should be("x")
+      selectElement.value should be("0")
+
+      select.kill()
+      p.listenersCount() should be(0)
+    }
+  }
+
+  "Select with multiple on" should {
+    "synchronise state with property changes (deprecated)" in {
       val options = Seq("A", "B", "C", "D", "E")
       val p = SeqProperty[String]("B")
 
-      val select = inputs.Select(p, options, inputs.Select.defaultLabel)().render
+      val select = (Select(p, options, Select.defaultLabel)(): @silent).render
 
       select.childElementCount should be(5)
       select.childNodes(0).asInstanceOf[org.scalajs.dom.html.Option].selected should be(false)
@@ -76,11 +151,11 @@ class SelectTest extends UdashFrontendTest {
       select.childNodes(4).asInstanceOf[org.scalajs.dom.html.Option].selected should be(false)
     }
 
-    "synchronise property with state changes" in {
+    "synchronise property with state changes (deprecated)" in {
       val options = Seq("A", "B", "C", "D", "E")
       val p = SeqProperty[String]("B")
 
-      val select = inputs.Select(p, options, inputs.Select.defaultLabel)().render
+      val select = (Select(p, options, Select.defaultLabel)(): @silent).render
 
       select.childElementCount should be(5)
 
@@ -114,6 +189,125 @@ class SelectTest extends UdashFrontendTest {
       p.get should contain("A")
       p.get should contain("C")
       p.get should contain("E")
+    }
+
+    "synchronise state with property changes" in {
+      val options = Seq("A", "B", "C", "D", "E")
+      val p = SeqProperty[String]("B")
+
+      val select = Select(p, options.toSeqProperty)(Select.defaultLabel)
+      val selectElement = select.render
+
+      selectElement.childElementCount should be(5)
+      selectElement.childNodes(0).asInstanceOf[org.scalajs.dom.html.Option].selected should be(false)
+      selectElement.childNodes(1).asInstanceOf[org.scalajs.dom.html.Option].selected should be(true)
+      selectElement.childNodes(2).asInstanceOf[org.scalajs.dom.html.Option].selected should be(false)
+      selectElement.childNodes(3).asInstanceOf[org.scalajs.dom.html.Option].selected should be(false)
+      selectElement.childNodes(4).asInstanceOf[org.scalajs.dom.html.Option].selected should be(false)
+
+      for (o <- options) {
+        p.set(Seq(o))
+        selectElement.childNodes(0).asInstanceOf[org.scalajs.dom.html.Option].selected should be(options.indexOf(o) == 0)
+        selectElement.childNodes(1).asInstanceOf[org.scalajs.dom.html.Option].selected should be(options.indexOf(o) == 1)
+        selectElement.childNodes(2).asInstanceOf[org.scalajs.dom.html.Option].selected should be(options.indexOf(o) == 2)
+        selectElement.childNodes(3).asInstanceOf[org.scalajs.dom.html.Option].selected should be(options.indexOf(o) == 3)
+        selectElement.childNodes(4).asInstanceOf[org.scalajs.dom.html.Option].selected should be(options.indexOf(o) == 4)
+      }
+
+      p.set(Seq("A", "B", "C", "D", "E"))
+      selectElement.childNodes(0).asInstanceOf[org.scalajs.dom.html.Option].selected should be(true)
+      selectElement.childNodes(1).asInstanceOf[org.scalajs.dom.html.Option].selected should be(true)
+      selectElement.childNodes(2).asInstanceOf[org.scalajs.dom.html.Option].selected should be(true)
+      selectElement.childNodes(3).asInstanceOf[org.scalajs.dom.html.Option].selected should be(true)
+      selectElement.childNodes(4).asInstanceOf[org.scalajs.dom.html.Option].selected should be(true)
+
+      p.clear()
+      selectElement.childNodes(0).asInstanceOf[org.scalajs.dom.html.Option].selected should be(false)
+      selectElement.childNodes(1).asInstanceOf[org.scalajs.dom.html.Option].selected should be(false)
+      selectElement.childNodes(2).asInstanceOf[org.scalajs.dom.html.Option].selected should be(false)
+      selectElement.childNodes(3).asInstanceOf[org.scalajs.dom.html.Option].selected should be(false)
+      selectElement.childNodes(4).asInstanceOf[org.scalajs.dom.html.Option].selected should be(false)
+    }
+
+    "synchronise property with state changes" in {
+      val options = Seq("A", "B", "C", "D", "E")
+      val p = SeqProperty[String]("B")
+
+      val select = Select(p, options.toSeqProperty)(Select.defaultLabel)
+      val selectElement = select.render
+
+      selectElement.childElementCount should be(5)
+
+      for (o <- options) {
+        selectElement.childNodes(0).asInstanceOf[org.scalajs.dom.html.Option].selected = options.indexOf(o) == 0
+        selectElement.childNodes(1).asInstanceOf[org.scalajs.dom.html.Option].selected = options.indexOf(o) == 1
+        selectElement.childNodes(2).asInstanceOf[org.scalajs.dom.html.Option].selected = options.indexOf(o) == 2
+        selectElement.childNodes(3).asInstanceOf[org.scalajs.dom.html.Option].selected = options.indexOf(o) == 3
+        selectElement.childNodes(4).asInstanceOf[org.scalajs.dom.html.Option].selected = options.indexOf(o) == 4
+        selectElement.onchange(null)
+        p.get should be(Seq(o))
+      }
+
+      selectElement.childNodes(0).asInstanceOf[org.scalajs.dom.html.Option].selected = false
+      selectElement.childNodes(1).asInstanceOf[org.scalajs.dom.html.Option].selected = true
+      selectElement.childNodes(2).asInstanceOf[org.scalajs.dom.html.Option].selected = false
+      selectElement.childNodes(3).asInstanceOf[org.scalajs.dom.html.Option].selected = true
+      selectElement.childNodes(4).asInstanceOf[org.scalajs.dom.html.Option].selected = false
+      selectElement.onchange(null)
+      p.get.size should be(2)
+      p.get should contain("B")
+      p.get should contain("D")
+
+      selectElement.childNodes(0).asInstanceOf[org.scalajs.dom.html.Option].selected = true
+      selectElement.childNodes(1).asInstanceOf[org.scalajs.dom.html.Option].selected = false
+      selectElement.childNodes(2).asInstanceOf[org.scalajs.dom.html.Option].selected = true
+      selectElement.childNodes(3).asInstanceOf[org.scalajs.dom.html.Option].selected = false
+      selectElement.childNodes(4).asInstanceOf[org.scalajs.dom.html.Option].selected = true
+      selectElement.onchange(null)
+      p.get.size should be(3)
+      p.get should contain("A")
+      p.get should contain("C")
+      p.get should contain("E")
+    }
+
+    "synchronise visible options" in {
+      val options = SeqProperty("A", "B", "C", "D", "E")
+      val p = SeqProperty[String]("B")
+
+      val select = Select(p, options)(Select.defaultLabel)
+      val selectElement = select.render
+
+      selectElement.childElementCount should be(5)
+      p.listenersCount() should be(5)
+
+      for ((o, idx) <- options.get.zipWithIndex) {
+        p.set(Seq(o))
+        selectElement.value should be(idx.toString)
+      }
+
+      options.remove(2, 3)
+      selectElement.childElementCount should be(2)
+      p.listenersCount() should be(2)
+      p.get should be(Seq.empty)
+
+      p.set(Seq("A", "B"))
+      options.prepend("1", "2")
+      selectElement.childElementCount should be(4)
+      p.listenersCount() should be(4)
+      p.get should be(Seq("A", "B"))
+
+      options.clear()
+      selectElement.childElementCount should be(0)
+      p.listenersCount() should be(0)
+      p.get should be(Seq.empty)
+
+      options.append("x", "y", "z")
+      selectElement.childElementCount should be(3)
+      p.listenersCount() should be(3)
+      p.get should be(Seq.empty)
+
+      select.kill()
+      p.listenersCount() should be(0)
     }
   }
 }
