2ca6841a33dd3bca2e63fe6979037b220b7275f7
Starzu <m.starzec@avsystem.com>
2018-05-29 09:34:47 +0200

    FileInput - API refactoring

--- core/frontend/src/main/scala/io/udash/bindings/inputs/FileInput.scala
+++ core/frontend/src/main/scala/io/udash/bindings/inputs/FileInput.scala
@@ -1,12 +1,38 @@
 package io.udash.bindings.inputs
 
 import io.udash._
-import org.scalajs.dom.{Element, Event, File}
 import org.scalajs.dom.html.{Input => JSInput}
-
-import scalatags.generic.Modifier
+import org.scalajs.dom.{Event, File}
+import scalatags.JsDom.Modifier
+import scalatags.JsDom.all._
 
 object FileInput {
+
+  /**
+    * Creates file input providing information about selected files.
+    *
+    * @param selectedFiles This property contains information about files selected by user.
+    * @param acceptMultipleFiles Accepts more than one file if true.
+    * @param inputName Input element name.
+    * @return
+    */
+  def apply(
+    selectedFiles: SeqProperty[File], acceptMultipleFiles: ReadableProperty[Boolean]
+  )(inputName: String, xs: Modifier*): InputBinding[JSInput] = {
+    new InputBinding[JSInput] {
+      private val in = input(
+        `type` := "file", name := inputName, xs,
+        nestedInterceptor((multiple := "multiple").attrIf(acceptMultipleFiles))
+      ).render
+
+      in.onchange = (ev: Event) => {
+        ev.preventDefault()
+        selectedFiles.replace(0, selectedFiles.size, (0 until in.files.length).map { idx => in.files(idx) }:_*)
+      }
+
+      override def render: JSInput = in
+    }
+  }
 
   /**
     * Creates file input providing information about selected files.
@@ -16,9 +42,10 @@ object FileInput {
     * @param selectedFiles This property contains information about files selected by user.
     * @return
     */
+  @deprecated("Use the constructor with InputBinding return type.", "0.7.0")
   def apply(
     inputName: String, acceptMultipleFiles: ReadableProperty[Boolean], selectedFiles: SeqProperty[File]
-  )(inputMds: Modifier[Element]*): JSInput = {
+  )(inputMds: Modifier*): JSInput = {
     import scalatags.JsDom.all._
 
     val inp = input(
