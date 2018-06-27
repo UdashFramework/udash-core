d6d7d9dd337cce0ae596cd5a5e2eaca904286c69
Mateusz Starzec <m.starzec@avsystem.com>
2018-06-27 12:30:43 +0200

    Remove `rpc` package object from shared module

--- rpc/shared/src/main/scala/io/udash/rpc/annotations.scala
+++ rpc/shared/src/main/scala/io/udash/rpc/annotations.scala
@@ -7,7 +7,5 @@ import scala.annotation.StaticAnnotation
 @deprecated("RPC annotation is no longer needed", "0.7.0")
 class RPC extends StaticAnnotation
 
-object `package` {
-  @deprecated("Use rpcName from com.avsystem.commons.rpc", "0.7.0")
-  type RPCName = rpcName
-}
+@deprecated("Use rpcName from com.avsystem.commons.rpc", "0.7.0")
+class RPCName(override val name: String) extends rpcName(name)
