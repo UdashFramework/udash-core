0a09de4886cf13ef0827bbb129469eb0500d8f9c
Mateusz Starzec <m.starzec@avsystem.com>
2018-07-05 12:14:12 +0200

    Decs improvements and code cleanup

--- core/frontend/src/main/scala/io/udash/routing/UrlChangeProvider.scala
+++ core/frontend/src/main/scala/io/udash/routing/UrlChangeProvider.scala
@@ -6,13 +6,13 @@ import io.udash.properties.MutableBufferRegistration
 import io.udash.utils.Registration
 import org.scalajs.dom
 import org.scalajs.dom.{Element, Location}
-import org.scalajs.dom.raw.HashChangeEvent
 
 import scala.scalajs.js
 
 /** Provides information about current URL. */
 trait UrlChangeProvider {
-  /** Enables all required event listeners. */
+  /** Enables all required event listeners.
+    * [[io.udash.Application]] initializes the provider automatically. */
   def initialize(): Unit
 
   /** Changes the URL part representing the frontend routing state. */
@@ -35,9 +35,7 @@ object WindowUrlFragmentChangeProvider extends UrlChangeProvider {
   private val callbacks: js.Array[Url => Unit] = js.Array()
 
   override def initialize(): Unit = {
-    window.onhashchange = (_: HashChangeEvent) => {
-      callbacks.foreach(_.apply(currentFragment))
-    }
+    window.onhashchange = _ => callbacks.foreach(_.apply(currentFragment))
   }
 
   override def onFragmentChange(callback: Url => Unit): Registration = {
@@ -61,6 +59,35 @@ object WindowUrlPathChangeProvider extends UrlChangeProvider {
 
   private val callbacks: js.Array[Url => Unit] = js.Array()
 
+  @inline
+  private def isSameOrigin(loc: Location, url: JSUrl): Boolean =
+    loc.protocol == url.protocol && loc.hostname == url.hostname && loc.port == url.port
+
+  @inline
+  private def isSamePath(loc: Location, url: JSUrl): Boolean =
+    loc.pathname == url.pathname && loc.search == url.search
+
+  @inline
+  private def isSameHash(loc: Location, url: JSUrl): Boolean =
+    loc.hash == url.hash
+
+  @inline
+  private def shouldIgnoreClick(
+    event: MouseEvent, target: Element, href: String,
+    samePath: Boolean, sameHash: Boolean, sameOrigin: Boolean
+  ): Boolean = {
+    // handle only links in the same browser card
+    event.button != 0 || event.metaKey || event.ctrlKey || event.shiftKey ||
+      // ignore click if default already prevented
+      event.defaultPrevented ||
+      // ignore special link types
+      target.hasAttribute("download") || target.getAttribute("rel") == "external" || href.contains("mailto:") ||
+      // ignore if links to different domain
+      !sameOrigin ||
+      // ignore if only the URL fragment changed, but path is the same
+      (samePath && !sameHash)
+  }
+
   override def initialize(): Unit = {
     window.document.addEventListener("click", (event: MouseEvent) => {
       def findLink(el: Node): Element =
@@ -68,30 +95,14 @@ object WindowUrlPathChangeProvider extends UrlChangeProvider {
         else if (el.nodeName.toLowerCase() == "a") el.asInstanceOf[Element]
         else findLink(el.parentNode)
 
-      @inline
-      def isSameOrigin(loc: Location, url: JSUrl): Boolean =
-        loc.protocol == url.protocol && loc.hostname == url.hostname && loc.port == url.port
-
-      @inline
-      def isSamePath(loc: Location, url: JSUrl): Boolean =
-        loc.pathname == url.pathname && loc.search == url.search
-
-      @inline
-      def isSameHash(loc: Location, url: JSUrl): Boolean =
-        loc.hash == url.hash
-
       val target = findLink(event.target.asInstanceOf[Node])
       if (target != null) {
         val href = target.getAttribute("href")
         val location = window.location
         val newUrl = new JSUrl(href, location.toString)
-        val (samePath, sameHash, sameOrigin) = {
+        val (samePath, sameHash, sameOrigin) =
           (isSamePath(location, newUrl), isSameHash(location, newUrl), isSameOrigin(location, newUrl))
-        }
-        val ignore = event.button != 0 || event.metaKey || event.ctrlKey || event.shiftKey ||
-          event.defaultPrevented || target.hasAttribute("download") ||
-          target.getAttribute("rel") == "external" || href.contains("mailto:") ||
-          !sameOrigin || (samePath && !sameHash)
+        val ignore = shouldIgnoreClick(event, target, href, samePath, sameHash, sameOrigin)
 
         if (!ignore) {
           if (!samePath) {
