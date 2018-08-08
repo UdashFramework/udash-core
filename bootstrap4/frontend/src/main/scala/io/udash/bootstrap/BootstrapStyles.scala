b7b86fdad62cb8913189d4ef66ceb1d3c7861c62
Mateusz Starzec <m.starzec@avsystem.com>
2018-08-08 11:24:20 +0200

    Separate file for BootstrapTags

--- bootstrap4/frontend/src/main/scala/io/udash/bootstrap/BootstrapStyles.scala
+++ bootstrap4/frontend/src/main/scala/io/udash/bootstrap/BootstrapStyles.scala
@@ -3,24 +3,6 @@ package io.udash.bootstrap
 import com.avsystem.commons.misc.{AbstractValueEnum, AbstractValueEnumCompanion, EnumCtx}
 import io.udash.css.CssStyleName
 
-object BootstrapTags {
-  import scalatags.JsDom.all._
-
-  val dataBackdrop = attr("data-backdrop")
-  val dataBind = attr("data-bind")
-  val dataDismiss = attr("data-dismiss")
-  val dataKeyboard = attr("data-keyboard")
-  val dataLabel = attr("data-label")
-  val dataParent = attr("data-parent")
-  val dataRide = attr("data-ride")
-  val dataShow = attr("data-show")
-  val dataSlide = attr("data-slide")
-  val dataSlideTo = attr("data-slide-to")
-  val dataTarget = attr("data-target")
-  val dataToggle = attr("data-toggle")
-
-}
-
 object BootstrapStyles {
   final class ResponsiveBreakpoint(val classMarker: String)(implicit enumCtx: EnumCtx) extends AbstractValueEnum
   object ResponsiveBreakpoint extends AbstractValueEnumCompanion[ResponsiveBreakpoint] {
---
+++ bootstrap4/frontend/src/main/scala/io/udash/bootstrap/BootstrapTags.scala
@@ -0,0 +1,18 @@
+package io.udash.bootstrap
+
+object BootstrapTags {
+  import scalatags.JsDom.all._
+
+  val dataBackdrop = attr("data-backdrop")
+  val dataBind = attr("data-bind")
+  val dataDismiss = attr("data-dismiss")
+  val dataKeyboard = attr("data-keyboard")
+  val dataLabel = attr("data-label")
+  val dataParent = attr("data-parent")
+  val dataRide = attr("data-ride")
+  val dataShow = attr("data-show")
+  val dataSlide = attr("data-slide")
+  val dataSlideTo = attr("data-slide-to")
+  val dataTarget = attr("data-target")
+  val dataToggle = attr("data-toggle")
+}
