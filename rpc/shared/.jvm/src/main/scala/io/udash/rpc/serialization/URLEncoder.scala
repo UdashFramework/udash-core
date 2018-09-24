8d395d8120513b43be7cffe6ebbe5f5f31617257
Mateusz Starzec <m.starzec@avsystem.com>
2018-09-24 12:19:43 +0200

    JVM URLEncoder improvements

--- rpc/shared/.jvm/src/main/scala/io/udash/rpc/serialization/URLEncoder.scala
+++ rpc/shared/.jvm/src/main/scala/io/udash/rpc/serialization/URLEncoder.scala
@@ -2,8 +2,8 @@ package io.udash.rpc.serialization
 
 object URLEncoder {
   def encode(query: String): String =
-    java.net.URLEncoder.encode(query, "UTF-8").replace("+", "%20")
+    new java.net.URI(null, null, query, null).toASCIIString
 
   def decode(query: String): String =
-    java.net.URLDecoder.decode(query, "UTF-8")
+    new java.net.URI(query).getSchemeSpecificPart
 }
