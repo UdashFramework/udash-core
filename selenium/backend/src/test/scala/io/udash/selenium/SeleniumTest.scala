d55805d0e7ae8806e9a7d8ba99b61a75835e4197
Mateusz Starzec <m.starzec@avsystem.com>
2018-09-25 10:07:25 +0200

    Headless firefox and higher timeouts for Travis

--- selenium/backend/src/test/scala/io/udash/selenium/SeleniumTest.scala
+++ selenium/backend/src/test/scala/io/udash/selenium/SeleniumTest.scala
@@ -12,11 +12,11 @@ import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
 
 abstract class SeleniumTest extends WordSpec with Matchers with BeforeAndAfterAll with Eventually {
   override implicit val patienceConfig: PatienceConfig =
-    PatienceConfig(scaled(Span(1, Seconds)), scaled(Span(5, Millis)))
+    PatienceConfig(scaled(Span(10, Seconds)), scaled(Span(5, Millis)))
 
 //  val driver: RemoteWebDriver = new ChromeDriver(new ChromeOptions().setHeadless(false))
-  val driver: RemoteWebDriver = new FirefoxDriver(new FirefoxOptions().setHeadless(false))
-  driver.manage().timeouts().implicitlyWait(200, TimeUnit.MILLISECONDS)
+  val driver: RemoteWebDriver = new FirefoxDriver(new FirefoxOptions().setHeadless(true))
+  driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS)
   driver.manage().window().setSize(new Dimension(1440, 800))
 
   private val server: ApplicationServer = new ApplicationServer(9999, "selenium/frontend/target/UdashStatics/WebContent")
