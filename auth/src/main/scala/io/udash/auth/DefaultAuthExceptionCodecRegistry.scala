9a3535ffb9aca019ed93f398535ba7a8994013f1
Mateusz Starzec <m.starzec@avsystem.com>
2018-11-26 08:26:10 +0100

    DefaultAuthExceptionCodecRegistry names

--- auth/src/main/scala/io/udash/auth/DefaultAuthExceptionCodecRegistry.scala
+++ auth/src/main/scala/io/udash/auth/DefaultAuthExceptionCodecRegistry.scala
@@ -4,6 +4,6 @@ import com.avsystem.commons.serialization.GenCodec
 import io.udash.rpc.serialization.DefaultExceptionCodecRegistry
 
 class DefaultAuthExceptionCodecRegistry extends DefaultExceptionCodecRegistry {
-  register(GenCodec.create((input) => new UnauthenticatedException(), exceptionWriter))
-  register(GenCodec.create((input) => new UnauthorizedException(), exceptionWriter))
+  register(GenCodec.create(input => new UnauthenticatedException(), exceptionWriter), "UnauthenticatedException")
+  register(GenCodec.create(input => new UnauthorizedException(), exceptionWriter), "UnauthorizedException")
 }
