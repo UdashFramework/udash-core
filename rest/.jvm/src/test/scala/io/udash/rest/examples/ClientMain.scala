d36df67b8299956f68a81e5c54ffd5af6d42fe6c
ghik <romeqjanoosh@gmail.com>
2019-01-23 13:34:55 +0100

    improved REST client example

--- rest/.jvm/src/test/scala/io/udash/rest/examples/ClientMain.scala
+++ rest/.jvm/src/test/scala/io/udash/rest/examples/ClientMain.scala
@@ -3,23 +3,25 @@ package rest.examples
 
 import io.udash.rest.DefaultRestClient
 
-import scala.concurrent.Await
 import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Success}
 
 object ClientMain {
   def main(args: Array[String]): Unit = {
-    val proxy = DefaultRestClient[UserApi]("http://localhost:9090/")
+    val proxy: UserApi = DefaultRestClient[UserApi]("http://localhost:9090/")
+
+    val result: Future[User] = proxy.createUser("Fred", 1990)
 
     // just for this example, normally it's not recommended
     import scala.concurrent.ExecutionContext.Implicits.global
 
-    val result = proxy.createUser("Fred", 1990).andThen {
+    result.onComplete {
       case Success(user) => println(s"User ${user.id} created")
       case Failure(cause) => cause.printStackTrace()
     }
 
     // just wait until future is complete so that main thread doesn't finish prematurely
-    Await.result(result, 10.seconds)
+    Await.ready(result, 10.seconds)
   }
 }
