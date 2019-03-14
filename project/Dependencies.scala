67e5b01e7e6a3faf4a064a7c63f1fe54d01e24f6
ghik <r.janusz@avsystem.com>
2019-03-14 18:39:27 +0100

    updated REST docs

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
   val scalaCssVersion = "0.5.5"
 
   val servletVersion = "4.0.1"
-  val avsCommonsVersion = "1.34.10-SNAPSHOT"
+  val avsCommonsVersion = "1.34.10"
 
   val atmosphereJSVersion = "2.3.8"
   val atmosphereVersion = "2.4.32"
