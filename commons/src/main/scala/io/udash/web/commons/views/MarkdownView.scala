489e29c0e012d78ec3e4d2db7b24bac079e79f80
FreexD <michalwoln@gmail.com>
2019-03-15 11:31:47 +0100

    [markdown] styles for error messages

--- commons/src/main/scala/io/udash/web/commons/views/MarkdownView.scala
+++ commons/src/main/scala/io/udash/web/commons/views/MarkdownView.scala
@@ -3,6 +3,9 @@ package io.udash.web.commons.views
 import com.avsystem.commons._
 import com.avsystem.commons.misc.AbstractCase
 import io.udash._
+import io.udash.bootstrap.alert.UdashAlert
+import io.udash.bootstrap.utils.BootstrapImplicits._
+import io.udash.bootstrap.utils.BootstrapStyles
 import io.udash.web.guide.markdown.{MarkdownPage, MarkdownPageRPC}
 import io.udash.web.guide.styles.MarkdownStyles
 
@@ -12,31 +15,54 @@ trait MarkdownPageState extends State {
   def page: MarkdownPage
 }
 
+final case class MarkdownModel(
+  content: String = "",
+  error: String = ""
+)
+object MarkdownModel extends HasModelPropertyCreator[MarkdownModel] {
+  implicit val blank: Blank[MarkdownModel] = Blank.Simple(apply())
+}
+
 final case class MarkdownPageViewFactory[T <: MarkdownPageState]()(
   rpc: MarkdownPageRPC
 ) extends AbstractCase with ViewFactory[T] {
   override def create(): (MarkdownView, MarkdownPresenter[T]) = {
-    val content: Property[String] = Property("")
-    (new MarkdownView(content), new MarkdownPresenter[T](content, rpc))
+    val model: ModelProperty[MarkdownModel] = ModelProperty.blank
+    (new MarkdownView(model), new MarkdownPresenter[T](model, rpc))
   }
 }
 
 final class MarkdownPresenter[T <: MarkdownPageState](
-  content: Property[String],
+  model: ModelProperty[MarkdownModel],
   rpc: MarkdownPageRPC
 ) extends Presenter[T] {
   override def handleState(state: T): Unit = {
+    model.set(MarkdownModel.blank.value)
     rpc.loadContent(state.page).onCompleteNow {
-      case Success(rawHtml) => content.set(rawHtml)
-      case Failure(exception) => content.set(exception.toString)
+      case Success(rawHtml) => model.subProp(_.content).set(rawHtml)
+      case Failure(exception) => model.subProp(_.error).set(exception.toString)
     }
   }
 }
 
-final class MarkdownView(content: ReadableProperty[String]) extends View {
+final class MarkdownView(model: ReadableModelProperty[MarkdownModel]) extends View {
   import io.udash.css.CssView._
   import scalatags.JsDom.all._
 
-  override val getTemplate: Modifier =
-    produce(content)(content => div(MarkdownStyles.markdownPage)(raw(content)).render)
+  override val getTemplate: Modifier = ISeq(
+    produce(model.roSubProp(_.error)) { error =>
+      error.opt.filter(_.nonEmpty).map(e =>
+        div(cls := "bootstrap")(
+          h1("Oops! Something went wrong :("),
+          p("An error occurred during rendering your page:"),
+          UdashAlert(alertStyle = BootstrapStyles.Color.Danger.toProperty)(e).render
+        ).render
+      ).toList
+    },
+    produce(model.roSubProp(_.content)) { content =>
+      content.opt.filter(_.nonEmpty).map(c =>
+        div(MarkdownStyles.markdownPage)(raw(c)).render
+      ).toList
+    }
+  )
 }
