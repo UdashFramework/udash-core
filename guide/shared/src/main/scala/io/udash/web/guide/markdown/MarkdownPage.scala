39cf9323f404c3b7c4085a233dbeea95c4691908
ghik <r.janusz@avsystem.com>
2019-04-01 17:40:43 +0200

    replaced REST docs with those originally migrated from commons-rest

--- guide/shared/src/main/scala/io/udash/web/guide/markdown/MarkdownPage.scala
+++ guide/shared/src/main/scala/io/udash/web/guide/markdown/MarkdownPage.scala
@@ -5,6 +5,7 @@ import com.avsystem.commons.misc.{AbstractValueEnum, AbstractValueEnumCompanion,
 final class MarkdownPage(val file: String)(implicit val ctx: EnumCtx) extends AbstractValueEnum
 object MarkdownPage extends AbstractValueEnumCompanion[MarkdownPage] {
   final val Intro: Value = new MarkdownPage("assets/pages/intro.md")
+  final val Rest: Value = new MarkdownPage("assets/pages/rest.md")
   final val I18n: Value = new MarkdownPage("assets/pages/ext/i18n.md")
   final val License: Value = new MarkdownPage("assets/pages/license.md")
 }
