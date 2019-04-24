daeac27b4be401fa47c79f529e7b99b22e4c719d
ghik <r.janusz@avsystem.com>
2019-04-24 13:22:07 +0200

    fixed async servlet timeout handling

--- rest/.jvm/src/test/scala/io/udash/rest/SttpRestCallTest.scala
+++ rest/.jvm/src/test/scala/io/udash/rest/SttpRestCallTest.scala
@@ -2,11 +2,13 @@ package io.udash
 package rest
 
 import com.softwaremill.sttp.SttpBackend
+import io.udash.rest.raw.HttpErrorException
 import io.udash.rest.raw.RawRest.HandleRequest
 
 import scala.concurrent.Future
+import scala.concurrent.duration._
 
-final class SttpRestCallTest extends ServletRestCallTest {
+trait SttpClientRestTest extends ServletBasedRestApiTest {
   implicit val backend: SttpBackend[Future, Nothing] = SttpRestClient.defaultBackend()
 
   def clientHandle: HandleRequest =
@@ -15,5 +17,22 @@ final class SttpRestCallTest extends ServletRestCallTest {
   override protected def afterAll(): Unit = {
     backend.close()
     super.afterAll()
+  }
+}
+
+class SttpRestCallTest extends SttpClientRestTest with RestApiTestScenarios {
+  test("too large binary request") {
+    val future = proxy.binaryEcho(Array.fill[Byte](maxPayloadSize + 1)(5))
+    val exception = future.failed.futureValue
+    assert(exception == HttpErrorException(413, "Payload is larger than maximum 1048576 bytes (1048577)"))
+  }
+}
+
+class ServletTimeoutTest extends SttpClientRestTest {
+  override def serverTimeout: FiniteDuration = 1.millisecond
+
+  test("rest method timeout") {
+    val exception = proxy.neverGet.failed.futureValue
+    assert(exception == HttpErrorException(500, "server operation timed out"))
   }
 }
--- rest/jetty/src/test/scala/io/udash/rest/jetty/JettyRestCallTest.scala
+++ rest/jetty/src/test/scala/io/udash/rest/jetty/JettyRestCallTest.scala
@@ -1,15 +1,15 @@
 package io.udash
 package rest.jetty
 
-import io.udash.rest.ServletRestCallTest
 import io.udash.rest.raw.RawRest.HandleRequest
+import io.udash.rest.{RestApiTestScenarios, ServletBasedRestApiTest}
 import org.eclipse.jetty.client.HttpClient
 
-final class JettyRestCallTest extends ServletRestCallTest {
+final class JettyRestCallTest extends ServletBasedRestApiTest with RestApiTestScenarios {
   val client: HttpClient = new HttpClient
 
   def clientHandle: HandleRequest =
-    JettyRestClient.asHandleRequest(client, s"$baseUrl/api", MaxPayloadSize)
+    JettyRestClient.asHandleRequest(client, s"$baseUrl/api", maxPayloadSize)
 
   override protected def beforeAll(): Unit = {
     super.beforeAll()
