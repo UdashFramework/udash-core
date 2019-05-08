74b6dc2fc1307af86a2fcc85699cfaacb0181d6b
ddworak <dawid.dworak@gmail.com>
2019-05-08 16:12:59 +0200

    Avoid copying listeners in ReadableProperty

--- core/src/main/scala/io/udash/properties/single/ReadableProperty.scala
+++ core/src/main/scala/io/udash/properties/single/ReadableProperty.scala
@@ -160,12 +160,10 @@ private[properties] trait AbstractReadableProperty[A] extends ReadableProperty[A
   protected[properties] override def fireValueListeners(): Unit = {
     val originalListeners = listeners.toSet
     CallbackSequencer().queue(s"${this.id.toString}:fireValueListeners", () => {
-      val t = get
-      val listenersCopy = CrossCollections.copyArray(listeners)
-      val oneTimeListenersCopy = CrossCollections.copyArray(oneTimeListeners)
+      val value = get
+      listeners.foreach { listener => if (originalListeners.contains(listener)) listener(value) }
+      oneTimeListeners.foreach(_.cancel())
       oneTimeListeners.clear()
-      oneTimeListenersCopy.foreach(_.cancel())
-      listenersCopy.foreach { listener => if (originalListeners.contains(listener)) listener(t) }
     })
   }
 
