a3b79082358bbd6d461dfe65e522a78ddde8f55f
ddworak <dawid.dworak@gmail.com>
2019-06-08 17:58:22 +0200

    RP applicative PoC

---
+++ core/cats/src/main/scala/io/udash/cats/CatsFun.scala
@@ -0,0 +1,35 @@
+package io.udash
+package cats
+
+import io.udash.properties.HasModelPropertyCreator
+
+
+object CatsFun extends App {
+  import _root_.cats._
+  import _root_.cats.implicits._
+  import io.udash.properties.Properties._
+
+  implicit val rpApplicative: Applicative[ReadableProperty] = new Applicative[ReadableProperty] {
+    override def pure[A](x: A): ReadableProperty[A] = x.toProperty
+    override def ap[A, B](ff: ReadableProperty[A => B])(fa: ReadableProperty[A]): ReadableProperty[B] =
+      ff.combine(fa)((f, a) => f(a))
+  }
+
+  case class Test(i: Int, s: String)
+  object Test extends HasModelPropertyCreator[Test]
+
+  val mp = ModelProperty(Test(4, "2"))
+
+  val r = (mp.roSubProp(_.i), mp.roSubProp(_.s)).mapN((i, s) => i+s)
+  r.listen(println, initUpdate = true)
+
+
+  mp.subProp(_.i).set(6)
+
+  mp.subProp(_.s).set("9")
+
+  CallbackSequencer().sequence {
+    mp.subProp(_.i).set(6)
+    mp.subProp(_.s).set("4")
+  }
+}
