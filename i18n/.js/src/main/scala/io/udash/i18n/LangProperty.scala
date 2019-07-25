3e1e2b2526d98b4d135d30adcdf5e9802857895e
ddworak <dawid.dworak@gmail.com>
2019-07-25 11:39:08 +0200

    Require only ReadableProperty for lang

--- i18n/.js/src/main/scala/io/udash/i18n/LangProperty.scala
+++
@@ -1,9 +0,0 @@
-package io.udash.i18n
-
-import io.udash._
-
-object LangProperty {
-  /** Helper for `LangProperty` instance creation. */
-  def apply(lang: Lang): LangProperty =
-    Property(lang)
-}
