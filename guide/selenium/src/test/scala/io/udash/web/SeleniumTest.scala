c43fbc88e90ba5c871f7482258f3449bfa1d8304
Dawid Dworak <dawid.dworak@gmail.com>
2019-08-23 13:33:01 +0200

    Remove Spring from guide (#377)
    * Remove Spring from guide
    
    Related to #376
    
    * Use non-default port for embedded Selenium guide server

--- guide/selenium/src/test/scala/io/udash/web/SeleniumTest.scala
+++ guide/selenium/src/test/scala/io/udash/web/SeleniumTest.scala
@@ -2,22 +2,21 @@ package io.udash.web
 
 import java.util.concurrent.TimeUnit
 
-import com.avsystem.commons.spring.HoconBeanDefinitionReader
-import io.udash.web.server.ApplicationServer
 import org.openqa.selenium.Dimension
+import org.openqa.selenium.firefox.{FirefoxDriver, FirefoxOptions}
 import org.openqa.selenium.remote.RemoteWebDriver
 import org.scalatest.concurrent.Eventually
 import org.scalatest.time.{Millis, Seconds, Span}
 import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, Matchers, WordSpec}
-import org.springframework.context.support.GenericApplicationContext
 
-trait ServerConfig {
+private trait ServerConfig {
   def init(): Unit
   def createUrl(part: String): String
   def destroy(): Unit
 }
 
-class ExternalServerConfig(urlPrefix: String) extends ServerConfig {
+// Doesn't launch embedded guide app server
+private final class ExternalServerConfig(urlPrefix: String) extends ServerConfig {
   require(!urlPrefix.endsWith("/"))
 
   override def createUrl(part: String): String = {
@@ -29,35 +28,30 @@ class ExternalServerConfig(urlPrefix: String) extends ServerConfig {
   override def destroy(): Unit = {}
 }
 
-class InternalServerConfig extends ServerConfig {
-  val guideCtx = Launcher.createApplicationContext()
+// Launches embedded guide server
+private final class InternalServerConfig extends ServerConfig {
+  private val server = Launcher.createApplicationServer()
 
-  override def init(): Unit =
-    guideCtx.getBean(classOf[ApplicationServer]).start()
+  override def init(): Unit = server.start()
 
-  override def destroy(): Unit = {
-    guideCtx.getBean(classOf[ApplicationServer]).stop()
-    guideCtx.close()
-  }
+  override def destroy(): Unit = server.stop()
 
   override def createUrl(part: String): String = {
     require(part.startsWith("/"))
-    s"http://127.0.0.2:${guideCtx.getBean(classOf[ApplicationServer]).port}$part"
+    s"http://127.0.0.2:${server.port}$part"
   }
 }
 
 abstract class SeleniumTest extends WordSpec with Matchers with BeforeAndAfterAll with BeforeAndAfterEach with Eventually {
   override implicit val patienceConfig: PatienceConfig = PatienceConfig(scaled(Span(10, Seconds)), scaled(Span(50, Millis)))
 
-  private val testingCtx = createTestingContext()
-
-  protected final val driver: RemoteWebDriver = testingCtx.getBean(classOf[RemoteWebDriver])
+  protected final val driver: RemoteWebDriver = new FirefoxDriver(new FirefoxOptions().setHeadless(true))
   driver.manage().timeouts().implicitlyWait(200, TimeUnit.MILLISECONDS)
   driver.manage().window().setSize(new Dimension(1440, 800))
 
   protected def url: String
 
-  private val server: ServerConfig = testingCtx.getBean(classOf[ServerConfig])
+  private val server: ServerConfig = new InternalServerConfig
 
   override protected def beforeAll(): Unit = {
     super.beforeAll()
@@ -73,13 +67,5 @@ abstract class SeleniumTest extends WordSpec with Matchers with BeforeAndAfterAl
     super.afterAll()
     server.destroy()
     driver.close()
-  }
-
-  private def createTestingContext(): GenericApplicationContext = {
-    val ctx = new GenericApplicationContext()
-    val bdr = new HoconBeanDefinitionReader(ctx)
-    bdr.loadBeanDefinitions("testing_beans.conf")
-    ctx.refresh()
-    ctx
   }
 }
