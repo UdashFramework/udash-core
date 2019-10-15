07ba710278de12475389bc98fcdbfa83128d7452
Scala Steward <me@scala-steward.org>
2019-10-15 17:02:33 +0200

    Update udash-jquery to 3.0.2

--- guide/guide/.js/src/main/scala/io/udash/web/guide/views/Versions.scala
+++ guide/guide/.js/src/main/scala/io/udash/web/guide/views/Versions.scala
@@ -2,6 +2,6 @@ package io.udash.web.guide.views
 
 object Versions {
   val udashVersion = "0.8.0"
-  val udashJQueryVersion = "3.0.1"
+  val udashJQueryVersion = "3.0.2"
   val highchartsVersion = "5.0.10"
 }
