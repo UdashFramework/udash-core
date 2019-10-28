94e0cb1e65a68329aa43bd1d03d7313dc7c092ce
gszeremeta <g.szeremeta@avsystem.com>
2019-10-28 15:45:51 +0100

    removed canRenderView and used requireWithFallback as default instead

--- auth/.js/src/main/scala/io/udash/auth/AuthView.scala
+++ auth/.js/src/main/scala/io/udash/auth/AuthView.scala
@@ -6,22 +6,20 @@ import scalatags.JsDom.all._
 trait AuthView {
   /** Renders provided `view` only if user context has required permissions. */
   def require(permission: PermissionCombinator, requireAuthenticated: Boolean = false)(view: => Modifier)(implicit userCtx: UserCtx): Modifier =
-    if (canRenderView(permission, requireAuthenticated)) view else ()
+    requireWithFallback(permission, requireAuthenticated)(view)()
 
   /** Renders provided `view` only if user is authenticated. */
   def requireAuthenticated(view: => Modifier)(implicit userCtx: UserCtx): Modifier =
     require(AllowAll, requireAuthenticated = true)(view)
 
-  /** Renders provided view if user is authenticated or fallback view otherwise */
+  /** Renders provided `view` if user context has required permissions or `fallback` view otherwise. */
   def requireWithFallback(permission: PermissionCombinator, requireAuthenticated: Boolean = false)
-    (view: => Modifier)(fallback: => Modifier)(implicit userCtx: UserCtx): Modifier =
-    if (canRenderView(permission, requireAuthenticated)) view else fallback
+    (view: => Modifier)(fallback: => Modifier = ())(implicit userCtx: UserCtx): Modifier =
+    if ((!requireAuthenticated || userCtx.isAuthenticated) && permission.check(userCtx)) view else fallback
 
+  /** Renders provided `view` if user is authenticated or `fallback` view otherwise. */
   def requireAuthenticatedWithFallback(view: => Modifier)(fallback: => Modifier)(implicit userCtx: UserCtx): Modifier =
     requireWithFallback(AllowAll, requireAuthenticated = true)(view)(fallback)
-
-  private def canRenderView(permission: PermissionCombinator, requireAuthenticated: Boolean)(implicit userCtx: UserCtx): Boolean =
-    (!requireAuthenticated || userCtx.isAuthenticated) && permission.check(userCtx)
 }
 
 object AuthView extends AuthView
