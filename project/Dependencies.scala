c35735e2334c7dcce9176ea1feb3a6423d8d7613
Scala Steward <me@scala-steward.org>
2019-12-13 23:12:41 +0100

    Update font-awesome to 5.12.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -35,7 +35,7 @@ object Dependencies {
   val typesafeConfigVersion = "1.4.0"
   val flexmarkVersion = "0.50.44"
   val logbackVersion = "1.2.3"
-  val fontAwesomeVersion = "5.10.1"
+  val fontAwesomeVersion = "5.12.0"
   val svg4everybodyVersion = "2.1.9"
 
   val scalatestVersion = "3.0.8"
