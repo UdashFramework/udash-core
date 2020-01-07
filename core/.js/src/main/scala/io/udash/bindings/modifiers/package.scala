463a954c9f161c7fd8fd244c82b45e8aa95e8f7c
ddworak <dawid.dworak@gmail.com>
2020-01-07 15:49:57 +0100

    Don't remove undefined elements

--- core/.js/src/main/scala/io/udash/bindings/modifiers/package.scala
+++ core/.js/src/main/scala/io/udash/bindings/modifiers/package.scala
@@ -2,6 +2,8 @@ package io.udash.bindings
 
 import org.scalajs.dom.Node
 
+import scala.scalajs.js
+
 package object modifiers {
   implicit class ElementExts(private val el: Node) extends AnyVal {
     def replaceChildren(oldChildren: Seq[Node], newChildren: Seq[Node]): Unit = {
@@ -10,7 +12,10 @@ package object modifiers {
         oldChildren.iterator.zip(newChildren.iterator).foreach { case (old, fresh) =>
           el.replaceChild(fresh, old)
         }
-        oldChildren.iterator.drop(newChildren.size).foreach(el.removeChild)
+        oldChildren.iterator.drop(newChildren.size).foreach { child =>
+          //todo can we avoid this check?
+          js.UndefOr.any2undefOrA(child).foreach(el.removeChild)
+        }
         newChildren.iterator.drop(oldChildren.size - 1).sliding(2)
           .foreach(s => if (s.size == 2) el.insertBefore(s(1), s(0).nextSibling))
       }
