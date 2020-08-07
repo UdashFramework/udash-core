366f2107eb4dc4e16e5007e7cf6f2b59b421ae29
Scala Steward <me@scala-steward.org>
2020-08-07 14:59:29 +0200

    Update async-http-client-backend-future, ... to 2.2.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
   val circeDerivationVersion = "0.13.0-M4" // Tests only
   val monixVersion = "3.2.2" // Tests only
 
-  val sttpVersion = "2.2.3"
+  val sttpVersion = "2.2.4"
 
   val scalaLoggingVersion = "3.9.2"
 
