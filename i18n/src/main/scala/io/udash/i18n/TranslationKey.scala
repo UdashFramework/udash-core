d67eb820de0a9681e26aa8046739fc03f2e691c6
attomir <attomir@gmail.com>
2020-09-23 15:01:55 +0200

    Turn TranslationKeys into case classes (#642)
    * Turn TranslationKey implementations into case classes
    
    TranslationKey0 has subclasses and they cannot be case classes themselves.
    For them the appropriate behavior is coded up manually.
    
    * Turn TranslationKey0 into a trait, extract SimpleTranslationKey0 from it
    
    * Minor
    
    * Convert arguments to string in ReducedTranslationKey
    
    * Very minor
    
    * Bring back TranslationKeys' equality and hashCode tests

--- i18n/src/main/scala/io/udash/i18n/TranslationKey.scala
+++ i18n/src/main/scala/io/udash/i18n/TranslationKey.scala
@@ -1,141 +1,119 @@
 package io.udash.i18n
 
-import com.avsystem.commons.serialization.{GenCodec, Input, Output}
+import com.avsystem.commons.misc.AbstractCase
+import com.avsystem.commons.serialization.HasGenCodec
 
 import scala.concurrent.Future
-import scala.util.Try
 
-sealed trait TranslationKey {
+sealed trait TranslationKey extends AbstractCase {
   def key: String
 }
 
-sealed class TranslationKey0(override val key: String) extends TranslationKey {
+sealed trait TranslationKey0 extends TranslationKey {
   def apply()(implicit provider: TranslationProvider, lang: Lang): Future[Translated] = provider.translate(key)
 }
 
-final class TranslationKey1[T](override val key: String) extends TranslationKey {
-  def apply(arg1: T): TranslationKey0 = new TranslationKey.ReducedTranslationKey(key, arg1)
+object TranslationKey0 extends HasGenCodec[TranslationKey0] {
+  def apply(key: String): TranslationKey0 = TranslationKey.SimpleTranslationKey0(key)
 }
 
-final class TranslationKey2[T1, T2](override val key: String) extends TranslationKey {
-  def apply(arg1: T1, arg2: T2): TranslationKey0 = new TranslationKey.ReducedTranslationKey(key, arg1, arg2)
+final case class TranslationKey1[T](key: String) extends TranslationKey {
+  def apply(arg1: T): TranslationKey0 = TranslationKey.reduced(key, arg1)
 }
 
-final class TranslationKey3[T1, T2, T3](override val key: String) extends TranslationKey {
-  def apply(arg1: T1, arg2: T2, arg3: T3): TranslationKey0 = new TranslationKey.ReducedTranslationKey(key, arg1, arg2, arg3)
+final case class TranslationKey2[T1, T2](key: String) extends TranslationKey {
+  def apply(arg1: T1, arg2: T2): TranslationKey0 = TranslationKey.reduced(key, arg1, arg2)
 }
 
-final class TranslationKey4[T1, T2, T3, T4](override val key: String) extends TranslationKey {
+final case class TranslationKey3[T1, T2, T3](key: String) extends TranslationKey {
+  def apply(arg1: T1, arg2: T2, arg3: T3): TranslationKey0 = TranslationKey.reduced(key, arg1, arg2, arg3)
+}
+
+final case class TranslationKey4[T1, T2, T3, T4](key: String) extends TranslationKey {
   def apply(arg1: T1, arg2: T2, arg3: T3, arg4: T4): TranslationKey0 =
-    new TranslationKey.ReducedTranslationKey(key, arg1, arg2, arg3, arg4)
+    TranslationKey.reduced(key, arg1, arg2, arg3, arg4)
 }
 
-final class TranslationKey5[T1, T2, T3, T4, T5](override val key: String) extends TranslationKey {
+final case class TranslationKey5[T1, T2, T3, T4, T5](key: String) extends TranslationKey {
   def apply(arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5): TranslationKey0 =
-    new TranslationKey.ReducedTranslationKey(key, arg1, arg2, arg3, arg4, arg5)
+    TranslationKey.reduced(key, arg1, arg2, arg3, arg4, arg5)
 }
 
-final class TranslationKey6[T1, T2, T3, T4, T5, T6](override val key: String) extends TranslationKey {
+final case class TranslationKey6[T1, T2, T3, T4, T5, T6](key: String) extends TranslationKey {
   def apply(arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6): TranslationKey0 =
-    new TranslationKey.ReducedTranslationKey(key, arg1, arg2, arg3, arg4, arg5, arg6)
+    TranslationKey.reduced(key, arg1, arg2, arg3, arg4, arg5, arg6)
 }
 
-final class TranslationKey7[T1, T2, T3, T4, T5, T6, T7](override val key: String) extends TranslationKey {
+final case class TranslationKey7[T1, T2, T3, T4, T5, T6, T7](key: String) extends TranslationKey {
   def apply(arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7): TranslationKey0 =
-    new TranslationKey.ReducedTranslationKey(key, arg1, arg2, arg3, arg4, arg5, arg6, arg7)
+    TranslationKey.reduced(key, arg1, arg2, arg3, arg4, arg5, arg6, arg7)
 }
 
-final class TranslationKey8[T1, T2, T3, T4, T5, T6, T7, T8](override val key: String) extends TranslationKey {
+final case class TranslationKey8[T1, T2, T3, T4, T5, T6, T7, T8](key: String) extends TranslationKey {
   def apply(arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7, arg8: T8): TranslationKey0 =
-    new TranslationKey.ReducedTranslationKey(key, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8)
+    TranslationKey.reduced(key, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8)
 }
 
-final class TranslationKey9[T1, T2, T3, T4, T5, T6, T7, T8, T9](override val key: String) extends TranslationKey {
+final case class TranslationKey9[T1, T2, T3, T4, T5, T6, T7, T8, T9](key: String) extends TranslationKey {
   def apply(arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7, arg8: T8, arg9: T9): TranslationKey0 =
-    new TranslationKey.ReducedTranslationKey(key, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9)
+    TranslationKey.reduced(key, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9)
 }
 
-final class TranslationKeyX(override val key: String) extends TranslationKey {
-  def apply(argv: Any*): TranslationKey0 = new TranslationKey.ReducedTranslationKey(key, argv: _*)
+final case class TranslationKeyX(key: String) extends TranslationKey {
+  def apply(argv: Any*): TranslationKey0 = TranslationKey.reduced(key, argv: _*)
 }
 
 object TranslationKey {
   def key(key: String): TranslationKey0 =
-    new TranslationKey0(key)
+    SimpleTranslationKey0(key)
 
   def key1[T](key: String): TranslationKey1[T] =
-    new TranslationKey1[T](key)
+    TranslationKey1[T](key)
 
   def key2[T1, T2](key: String): TranslationKey2[T1, T2] =
-    new TranslationKey2[T1, T2](key)
+    TranslationKey2[T1, T2](key)
 
   def key3[T1, T2, T3](key: String): TranslationKey3[T1, T2, T3] =
-    new TranslationKey3[T1, T2, T3](key)
+    TranslationKey3[T1, T2, T3](key)
 
   def key4[T1, T2, T3, T4](key: String): TranslationKey4[T1, T2, T3, T4] =
-    new TranslationKey4[T1, T2, T3, T4](key)
+    TranslationKey4[T1, T2, T3, T4](key)
 
   def key5[T1, T2, T3, T4, T5](key: String): TranslationKey5[T1, T2, T3, T4, T5] =
-    new TranslationKey5[T1, T2, T3, T4, T5](key)
+    TranslationKey5[T1, T2, T3, T4, T5](key)
 
   def key6[T1, T2, T3, T4, T5, T6](key: String): TranslationKey6[T1, T2, T3, T4, T5, T6] =
-    new TranslationKey6[T1, T2, T3, T4, T5, T6](key)
+    TranslationKey6[T1, T2, T3, T4, T5, T6](key)
 
   def key7[T1, T2, T3, T4, T5, T6, T7](key: String): TranslationKey7[T1, T2, T3, T4, T5, T6, T7] =
-    new TranslationKey7[T1, T2, T3, T4, T5, T6, T7](key)
+    TranslationKey7[T1, T2, T3, T4, T5, T6, T7](key)
 
   def key8[T1, T2, T3, T4, T5, T6, T7, T8](key: String): TranslationKey8[T1, T2, T3, T4, T5, T6, T7, T8] =
-    new TranslationKey8[T1, T2, T3, T4, T5, T6, T7, T8](key)
+    TranslationKey8[T1, T2, T3, T4, T5, T6, T7, T8](key)
 
   def key9[T1, T2, T3, T4, T5, T6, T7, T8, T9](key: String): TranslationKey9[T1, T2, T3, T4, T5, T6, T7, T8, T9] =
-    new TranslationKey9[T1, T2, T3, T4, T5, T6, T7, T8, T9](key)
+    TranslationKey9[T1, T2, T3, T4, T5, T6, T7, T8, T9](key)
 
   def keyX(key: String): TranslationKeyX =
-    new TranslationKeyX(key)
+    TranslationKeyX(key)
 
   def untranslatable(key: String): TranslationKey0 =
-    new Untranslatable(key)
+    Untranslatable(key)
 
-  private[i18n] final class ReducedTranslationKey(key: String, val argv: Any*) extends TranslationKey0(key) {
-    override def apply()(implicit provider: TranslationProvider, lang: Lang): Future[Translated] = {
+  private[i18n] def reduced(key: String, argv: Any*): ReducedTranslationKey =
+    ReducedTranslationKey(key, argv.map(_.toString): _*)
+
+  private[i18n] final case class SimpleTranslationKey0(key: String) extends TranslationKey0
+
+  private[i18n] final case class ReducedTranslationKey(key: String, argv: String*) extends TranslationKey0 {
+    override def apply()(implicit provider: TranslationProvider, lang: Lang): Future[Translated] =
       provider.translate(key, argv: _*)
-    }
+    // default toString puts argv inside "WrappedArray()"
+    override def toString(): String = s"$productPrefix($key,${argv.mkString(",")})"
   }
 
-  private[i18n] final class Untranslatable(key: String) extends TranslationKey0(key) {
+  private[i18n] final case class Untranslatable(key: String) extends TranslationKey0 {
     override def apply()(implicit provider: TranslationProvider, lang: Lang): Future[Translated] =
       Future.successful(Translated(key))
   }
-}
-
-object TranslationKey0 {
-  implicit val codec: GenCodec[TranslationKey0] = GenCodec.create[TranslationKey0](
-    (input: Input) => {
-      Try(input.readList()).toOption match {
-        case Some(list) =>
-          val key = list.nextElement().readSimple().readString()
-          val items = list.iterator(_.readSimple().readString()).toList
-          list.skipRemaining()
-          if (items.nonEmpty) new TranslationKey.ReducedTranslationKey(key, items: _*)
-          else TranslationKey.key(key)
-        case None =>
-          TranslationKey.untranslatable(input.readSimple().readString())
-      }
-    },
-    (output: Output, value: TranslationKey0) => {
-      value match {
-        case reduced: TranslationKey.ReducedTranslationKey =>
-          val data = output.writeList()
-          data.writeElement().writeSimple().writeString(value.key)
-          reduced.argv.foreach(item => data.writeElement().writeSimple().writeString(item.toString))
-          data.finish()
-        case untranslatable: TranslationKey.Untranslatable =>
-          output.writeSimple().writeString(untranslatable.key)
-        case std: TranslationKey =>
-          val data = output.writeList()
-          data.writeElement().writeSimple().writeString(std.key)
-          data.finish()
-      }
-    }
-  )
 }
