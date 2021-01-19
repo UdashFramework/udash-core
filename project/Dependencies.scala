43e83cf2491ff4d588b214b1d076b39938d13f7c
Scala Steward <me@scala-steward.org>
2021-01-19 22:07:52 +0100

    Update websocket-server to 9.4.36.v20210114

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,7 +30,7 @@ object Dependencies {
 
   val scalaLoggingVersion = "3.9.2"
 
-  val jettyVersion = "9.4.35.v20201120"
+  val jettyVersion = "9.4.36.v20210114"
   val typesafeConfigVersion = "1.4.1"
   val flexmarkVersion = "0.62.2"
   val logbackVersion = "1.2.3"
