b99a16f0de11b08e9cfed461cfd57551167a1702
Scala Steward <me@scala-steward.org>
2021-01-23 01:35:44 +0100

    Update bootstrap to 4.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -41,7 +41,7 @@ object Dependencies {
   val scalatestVersion = "3.2.3"
   val bootstrapVersion = "3.3.7-1"
   val bootstrapDatepickerVersion = "4.17.47"
-  val bootstrap4Version = "4.1.3"
+  val bootstrap4Version = "4.6.0"
   val bootstrap4DatepickerVersion = "5.1.2"
   val momentJsVersion = "2.24.0"
 
