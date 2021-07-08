9af53104885c5c099788877b2afe80473b862230
ghik <r.janusz@avsystem.com>
2021-07-08 10:07:32 +0200

    test added

--- rest/src/test/scala/io/udash/rest/raw/ServerImplApiTest.scala
+++ rest/src/test/scala/io/udash/rest/raw/ServerImplApiTest.scala
@@ -1,7 +1,9 @@
 package io.udash.rest.raw
 
 import com.avsystem.commons.Promise
+import com.avsystem.commons.serialization.json.JsonStringOutput
 import io.udash.rest.SomeServerApiImpl
+import io.udash.rest.openapi.Info
 import org.scalactic.source.Position
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.funsuite.AnyFunSuite
@@ -26,4 +28,49 @@ class ServerImplApiTest extends AnyFunSuite with ScalaFutures {
     assertRawExchange(request, response)
   }
 
+  test("openapi") {
+    val openapi = SomeServerApiImpl.openapiMetadata.openapi(Info("Test API", "0.1"))
+    val json = JsonStringOutput.writePretty(openapi)
+
+    assert(json ==
+      """{
+        |  "openapi": "3.0.2",
+        |  "info": {
+        |    "title": "Test API",
+        |    "version": "0.1"
+        |  },
+        |  "paths": {
+        |    "/thingy": {
+        |      "get": {
+        |        "operationId": "thingy",
+        |        "parameters": [
+        |          {
+        |            "name": "param",
+        |            "in": "query",
+        |            "required": true,
+        |            "explode": false,
+        |            "schema": {
+        |              "type": "integer",
+        |              "format": "int32"
+        |            }
+        |          }
+        |        ],
+        |        "responses": {
+        |          "200": {
+        |            "description": "Success",
+        |            "content": {
+        |              "application/json": {
+        |                "schema": {
+        |                  "type": "string"
+        |                }
+        |              }
+        |            }
+        |          }
+        |        }
+        |      }
+        |    }
+        |  },
+        |  "components": {}
+        |}""".stripMargin)
+  }
 }
