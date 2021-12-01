baacae2294f9b29742fe09266f19d589d65cda97
Scala Steward <me@scala-steward.org>
2021-12-01 05:10:59 +0100

    Update commons-analyzer, commons-core, ... to 2.5.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
   val scalaCssVersion = "0.7.0"
 
   val servletVersion = "4.0.1"
-  val avsCommonsVersion = "2.5.0"
+  val avsCommonsVersion = "2.5.1"
 
   val atmosphereJSVersion = "3.0.4"
   val atmosphereVersion = "2.7.3"
