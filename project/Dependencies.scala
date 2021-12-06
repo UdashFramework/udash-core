0816e7b2bbd386fb47e00ae277c9d2d31d650b94
Scala Steward <me@scala-steward.org>
2021-12-06 10:57:57 +0100

    Update scalacss:core to 1.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
 
   val scalaJsDomVersion = "1.2.0"
   val scalaTagsVersion = "0.9.4"
-  val scalaCssVersion = "0.7.0"
+  val scalaCssVersion = "1.0.0"
 
   val servletVersion = "4.0.1"
   val avsCommonsVersion = "2.5.1"
