9f96f78be30800f55ae28bd1c775fb333fbb5312
Dawid Dworak <dawid.dworak@gmail.com>
2021-12-23 13:54:25 +0100

    Attempt to fix flaky port binding in selenium

--- guide/selenium/src/test/scala/io/udash/web/SeleniumTest.scala
+++ guide/selenium/src/test/scala/io/udash/web/SeleniumTest.scala
@@ -48,7 +48,9 @@ private final class InternalServerConfig extends ServerConfig {
 abstract class SeleniumTest extends AnyWordSpec with Matchers with BeforeAndAfterAll with BeforeAndAfterEach with Eventually {
   override implicit val patienceConfig: PatienceConfig = PatienceConfig(scaled(Span(10, Seconds)), scaled(Span(50, Millis)))
 
-  WebDriverManager.firefoxdriver().setup()
+  private val driverManager = WebDriverManager.firefoxdriver()
+  driverManager.config().setServerPort(0)
+  driverManager.setup()
   protected final val driver: RemoteWebDriver = new FirefoxDriver(new FirefoxOptions().setHeadless(true))
   driver.manage().timeouts().implicitlyWait(Duration.ofMillis(200))
   driver.manage().window().setSize(new Dimension(1440, 800))
@@ -74,6 +76,6 @@ abstract class SeleniumTest extends AnyWordSpec with Matchers with BeforeAndAfte
   override protected def afterAll(): Unit = {
     super.afterAll()
     server.destroy()
-    driver.quit()
+    driverManager.quit(driver)
   }
 }
