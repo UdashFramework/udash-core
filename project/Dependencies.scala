4f78c398eedba7c7ccb196610d2a1e5c6a22e98b
Scala Steward <me@scala-steward.org>
2022-01-19 20:31:58 +0100

    Update scalatags to 0.11.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   val jqueryWrapperVersion = "3.2.0"
 
   val scalaJsDomVersion = "2.1.0"
-  val scalaTagsVersion = "0.11.0"
+  val scalaTagsVersion = "0.11.1"
   val scalaCssVersion = "1.0.0"
 
   val servletVersion = "4.0.1"
