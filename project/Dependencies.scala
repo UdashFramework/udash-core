534913343bf627d0bb1abb37f2b9dd04441366d4
Scala Steward <me@scala-steward.org>
2022-01-24 11:06:58 +0100

    Update scalatest to 3.2.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -36,7 +36,7 @@ object Dependencies {
   val fontAwesomeVersion = "5.10.1"
   val svg4everybodyVersion = "2.1.9"
 
-  val scalatestVersion = "3.2.10"
+  val scalatestVersion = "3.2.11"
   val bootstrap4Version = "4.1.3"
   val bootstrap4DatepickerVersion = "5.39.0"
   val momentJsVersion = "2.29.1"
