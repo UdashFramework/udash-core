25b0c900bb33352073761ef567d7178405d161d3
Scala Steward <me@scala-steward.org>
2022-02-02 05:08:42 +0100

    Update atmosphere-runtime to 2.7.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   val avsCommonsVersion = "2.5.1"
 
   val atmosphereJSVersion = "3.0.4"
-  val atmosphereVersion = "2.7.4"
+  val atmosphereVersion = "2.7.5"
 
   val upickleVersion = "1.5.0" // Tests only
   val circeVersion = "0.14.1" // Tests only
