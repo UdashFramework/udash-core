7163069b3676553f60525050172c064afe10c451
Dawid Dworak <dawid.dworak@gmail.com>
2022-02-24 16:26:24 +0100

    Fix content producing in DemoComponent

--- guide/homepage/.js/src/main/scala/io/udash/web/homepage/components/demo/DemoComponent.scala
+++ guide/homepage/.js/src/main/scala/io/udash/web/homepage/components/demo/DemoComponent.scala
@@ -1,6 +1,6 @@
 package io.udash.web.homepage.components.demo
 
-import com.avsystem.commons.SharedExtensions
+import com.avsystem.commons.SharedExtensions._
 import io.udash._
 import io.udash.web.commons.components.CodeBlock
 import io.udash.web.commons.components.CodeBlock.Prism
@@ -9,51 +9,42 @@ import io.udash.web.commons.views.{Component, Image}
 import io.udash.web.homepage.Context._
 import io.udash.web.homepage.IndexState
 import io.udash.web.homepage.styles.partials.{DemoStyles, HomepageStyles}
-import io.udash.wrappers.jquery._
 import org.scalajs.dom.Element
 import scalatags.JsDom.all._
 
 class DemoComponent(url: Property[IndexState]) extends Component {
 
-  private val fiddleContainer = div(DemoStyles.demoFiddle).render
-  private val jqFiddleContainer = jQ(fiddleContainer)
-
   private val template = div(DemoStyles.demoComponent)(
     Image("laptop.png", "", DemoStyles.laptopImage),
     div(DemoStyles.demoBody)(
       ul(DemoStyles.demoTabs)(
-        DemoComponent.demoEntries.map(entry =>
+        DemoComponent.demoEntries.iterator.map { case (state, entry) =>
           li(DemoStyles.demoTabsItem)(
             a(
               DemoStyles.demoTabsLink,
-              href := entry.targetState.url,
-              (attr(Attributes.data(Attributes.Active)) := "true").attrIf(url.transform(_ == entry.targetState))
+              href := state.url,
+              (attr(Attributes.data(Attributes.Active)) := "true").attrIf(url.transform(_ == state))
             )(entry.name)
           )
-        )
+        }.toSeq
       ),
-      fiddleContainer
+      div(DemoStyles.demoFiddle)(
+        produce(url) { state =>
+          DemoComponent.demoEntries(state).fiddle.setup(Prism.highlightAllUnder)
+        }
+      )
     )
   ).render
-
-  url.listen(onUrlChange, initUpdate = true)
-
-  private def onUrlChange(update: IndexState): Unit = {
-    val entryOption = DemoComponent.demoEntries.find(_.targetState == update)
-    val entry = entryOption.getOrElse(DemoComponent.demoEntries.head)
-
-    jqFiddleContainer.html(entry.fiddle)
-    Prism.highlightAllUnder(fiddleContainer)
-  }
 
   override def getTemplate: Modifier = template
 }
 
-object DemoComponent extends SharedExtensions {
+object DemoComponent {
 
   def code(): Element = {
     div(
       style := "display: grid; grid-template-columns: 60% 40%",
+      height := 100.pct,
       div(textAlign.left, backgroundColor := "#f5f2f0")(
         CodeBlock.lines(HelloDemo.source.linesIterator.drop(1).map(_.drop(2)).toSeq.dropRight(1).iterator)(HomepageStyles)
       ),
@@ -61,11 +52,11 @@ object DemoComponent extends SharedExtensions {
     ).render
   }
 
-  def demoEntries: Seq[DemoEntry] = Seq(
-    DemoEntry("Hello World", IndexState(Option("hello")), code()),
-    DemoEntry("Properties", IndexState(Option("properties")), div().render),
-    DemoEntry("Validation", IndexState(Option("validation")), div().render),
-    DemoEntry("i18n", IndexState(Option("i18n")), div().render),
+  def demoEntries: Map[IndexState, DemoEntry] = Map(
+    IndexState(Option("hello")) -> DemoEntry("Hello World", code()),
+    IndexState(Option("properties")) -> DemoEntry("Properties", div().render),
+    IndexState(Option("validation")) -> DemoEntry("Validation", div().render),
+    IndexState(Option("i18n")) -> DemoEntry("i18n", div().render),
   )
 
   private object HelloDemo {
--- guide/homepage/.js/src/main/scala/io/udash/web/homepage/components/demo/DemoEntry.scala
+++ guide/homepage/.js/src/main/scala/io/udash/web/homepage/components/demo/DemoEntry.scala
@@ -1,6 +1,5 @@
 package io.udash.web.homepage.components.demo
 
-import io.udash.web.homepage.IndexState
 import org.scalajs.dom.Element
 
-case class DemoEntry(name: String, targetState: IndexState, fiddle: Element)
+case class DemoEntry(name: String, fiddle: Element)
