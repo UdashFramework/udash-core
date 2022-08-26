f2aea33856503bd8c7cb4d95bf0ed0fd738bafa0
Scala Steward <scala_steward@virtuslab.com>
2022-08-26 18:10:45 +0000

    Update client3:async-http-client-backend-future, ... to 3.7.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
   val circeDerivationVersion = "0.13.0-M5" // Tests only
   val monixVersion = "3.4.1" // Tests only
 
-  val sttpVersion = "3.7.1"
+  val sttpVersion = "3.7.6"
 
   val scalaLoggingVersion = "3.9.5"
 
