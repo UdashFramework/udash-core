0038ae6af434c811fa1ea401c1b0f7263e2a0d2a
Scala Steward <scala_steward@virtuslab.com>
2022-12-20 19:43:14 +0000

    Update jetty-client, jetty-rewrite, ... to 11.0.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
 
   val scalaLoggingVersion = "3.9.5"
 
-  val jettyVersion = "9.4.50.v20221201"
+  val jettyVersion = "11.0.13"
   val typesafeConfigVersion = "1.4.2"
   val flexmarkVersion = "0.64.0"
   val logbackVersion = "1.3.5"
