5f9215c0d517f8ed23eb28fec3398ecfaa7a1905
Scala Steward <scala_steward@virtuslab.com>
2023-01-23 22:23:03 +0000

    Update webdrivermanager to 5.3.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -43,7 +43,7 @@ object Dependencies {
   val momentJsVersion = "2.29.4"
 
   val seleniumVersion = "4.7.2"
-  val webDriverManagerVersion = "5.3.1"
+  val webDriverManagerVersion = "5.3.2"
   val scalaJsBenchmarkVersion = "0.10.0"
 
   val compilerPlugins = Def.setting(Seq(
