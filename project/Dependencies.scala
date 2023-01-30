a2dd5e880de874f01428514e3343c47c0ea153f7
Scala Steward <scala_steward@virtuslab.com>
2023-01-30 00:58:48 +0000

    Update atmosphere-runtime to 2.7.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   val avsCommonsVersion = "2.7.7"
 
   val atmosphereJSVersion = "3.1.3"
-  val atmosphereVersion = "2.7.7"
+  val atmosphereVersion = "2.7.8"
 
   val upickleVersion = "2.0.0" // Tests only
   val circeVersion = "0.14.3" // Tests only
