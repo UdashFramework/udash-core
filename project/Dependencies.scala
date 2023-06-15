ea9e97313dfbf48f69b4dbd2429140353b0e6acf
Dawid Dworak <dawid.dworak@gmail.com>
2023-06-15 15:20:28 +0200

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -44,7 +44,6 @@ object Dependencies {
 
   val seleniumVersion = "4.10.0"
   val webDriverManagerVersion = "5.3.3"
-
   val scalaJsBenchmarkVersion = "0.10.0"
 
   val compilerPlugins = Def.setting(Seq(
