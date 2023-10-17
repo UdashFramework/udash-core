6494dc1c949a12f228182964a504e075247d3485
Scala Steward <scala_steward@virtuslab.com>
2023-10-17 05:00:56 +0000

    Update jetty-servlet to 11.0.17

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
 
   val scalaLoggingVersion = "3.9.5"
 
-  val jettyVersion = "10.0.17"
+  val jettyVersion = "11.0.17"
   val typesafeConfigVersion = "1.4.2"
   val flexmarkVersion = "0.64.8"
   val logbackVersion = "1.3.11"
