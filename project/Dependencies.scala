6eed6f00f4d24942d856b873e1ae4cf49706d3c4
Dawid Dworak <dawid.dworak@gmail.com>
2024-03-05 13:38:41 +0100

    Fix imports

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,4 +1,7 @@
+import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport.*
+import org.scalajs.jsdependencies.sbtplugin.JSDependenciesPlugin.autoImport.*
 import org.scalajs.jsdependencies.sbtplugin.JSModuleID
+import sbt.*
 import sbt.Keys.scalaVersion
 
 object Dependencies {
