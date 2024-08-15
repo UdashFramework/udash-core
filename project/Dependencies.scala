a9e6965ede2c6479c7af673aad1686919b2d5f9c
Scala Steward <scala_steward@virtuslab.com>
2024-08-15 20:52:46 +0000

    Update monix to 3.9.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
   val circeDerivationVersion = "0.13.0-M5" // Tests only
   val monixVersion = "3.4.1" // udash-rest only
 
-  val sttpVersion = "3.9.7"
+  val sttpVersion = "3.9.8"
 
   val scalaLoggingVersion = "3.9.5"
 
