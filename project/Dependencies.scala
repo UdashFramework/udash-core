7ecd277a7823a7b9598790b71ea1eaac3751fd00
Scala Steward <scala_steward@virtuslab.com>
2025-07-11 18:28:57 +0000

    Update typesafe:config to 1.4.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,7 @@ object Dependencies {
   val scalaLoggingVersion = "3.9.5"
 
   val jettyVersion = "12.0.23"
-  val typesafeConfigVersion = "1.4.3"
+  val typesafeConfigVersion = "1.4.4"
   val flexmarkVersion = "0.64.8"
   val logbackVersion = "1.3.15"
   val janinoVersion = "3.1.12"
